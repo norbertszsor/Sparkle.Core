6bd02314016fa1fb930e5fc2223da80919741e7f
norbertszsor <47736350+norbertszsor@users.noreply.github.com>
2023-08-30 19:59:09 +0200

    test

--- Sparkle.Api/Infrastructure/Seeder.cs
+++ Sparkle.Api/Infrastructure/Seeder.cs
@@ -101,6 +101,8 @@ namespace Sparkle.Api.Infrastructure
                 throw ThrowHelper.Throw<Seeder>("Connection string is empty.");
             }
 
+            throw ThrowHelper.Throw<Seeder>(AppContext.BaseDirectory+"-"+Directory.GetCurrentDirectory());
+
             SQLiteConnection.CreateFile(context.Connection.DataSource);
 
             using (await context.BeginTransactionAsync())
